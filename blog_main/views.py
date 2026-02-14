da2092ec1755e56f4ba36add4a59074fe7e20687
shashikumar <kshasikumar65@gmail.com>
2026-02-14 21:58:49 +0530

    blogs and search feature

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from assignments.models import About
 from blogs.models import Category,Blog
 
 def home(request):
@@ -8,10 +9,15 @@ def home(request):
     featured_posts = Blog.objects.filter(is_featured=True,status ='Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured=False , status ='Published').order_by('updated_at')
 
+    try:
+        about = About.objects.get()
+    except:
+        about= None
 
     context = {
         
         'featured_posts' : featured_posts,
-        'posts' : posts
+        'posts' : posts,
+        'about' : about
     }
     return render(request,'home.html',context)
